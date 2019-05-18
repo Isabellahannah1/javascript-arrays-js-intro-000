079a4eeb16131e33d8ad80077dd4d50ee1be743b
Isabella Hannah <isabellahannah735@gmail.com>
2019-05-18 20:16:00 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -37,4 +37,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
   return array;
 }
 
-function 
+function removeElementFromEndOfArray(array){
+  chocolateBars.slice(0, chocolateBars.length - 1);
+  return chocolateBars;
+}
